251bfeb351fc382c90bfdc3e3e6f867570e4280a
yuly <2765352250@163.com>
2026-02-12 01:21:43 +0800

    npx hardhat run .\scripts\studyEthers.js

--- scripts/studyEthers.js
+++ scripts/studyEthers.js
@@ -1,8 +1,12 @@
 import { ethers } from "ethers";
-const ALCHEMY_MAINNET_URL = 'https://sepolia.infura.io/v3/b526f584bc35450aa15514a783816180';
-const provider = new ethers.JsonRpcProvider(ALCHEMY_MAINNET_URL)
+const Ethereum_MainnetL = 'https://mainnet.infura.io/v3/b526f584bc35450aa15514a783816180';
+const Ethereum_SepoliaL = 'https://sepolia.infura.io/v3/b526f584bc35450aa15514a783816180';
+const providerMainnetL = new ethers.JsonRpcProvider(Ethereum_MainnetL)
+const providerSepoliaL = new ethers.JsonRpcProvider(Ethereum_SepoliaL)
 const main = async () => {
-    const balance = await provider.getBalance(`vitalik.eth`);
-    console.log(`ethers.eth: ${ethers.formatEther(balance)} ETH`);
+    // const balanceMainnetL = await providerMainnetL.getBalance(`vitalik.eth`);
+    const balanceSepoliaL = await providerSepoliaL.getBalance(`0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045`);
+    // console.log(`Balance Mainnet: ${ethers.formatEther(balanceMainnetL)} ETH`);
+    console.log(`Balance Sepolia: ${ethers.formatEther(balanceSepoliaL)} ETH`);
 }
-main();
+main()
