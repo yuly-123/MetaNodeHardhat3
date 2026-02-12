a37c5bcb0bc794312c67279f9ed36217565f7c16
yuly <2765352250@163.com>
2026-02-12 16:48:12 +0800

    链上交互

--- scripts/studyEthers.js
+++ scripts/studyEthers.js
@@ -3,6 +3,7 @@ const Ethereum_MainnetL = 'https://mainnet.infura.io/v3/b526f584bc35450aa15514a7
 const Ethereum_SepoliaL = 'https://sepolia.infura.io/v3/b526f584bc35450aa15514a783816180';
 const providerMainnetL = new ethers.JsonRpcProvider(Ethereum_MainnetL)
 const providerSepoliaL = new ethers.JsonRpcProvider(Ethereum_SepoliaL)
+
 const main = async () => {
     // const balanceMainnetL = await providerMainnetL.getBalance(`vitalik.eth`);
     const balanceSepoliaL = await providerSepoliaL.getBalance(`0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045`);
