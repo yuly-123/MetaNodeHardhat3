d440a5420229bd03e6913327e1208e22b8fe980e
yuly <2765352250@163.com>
2026-02-18 11:37:20 +0800

    合约交互,过滤事件

--- scripts/studyContractOn.js
+++ scripts/studyContractOn.js
@@ -23,5 +23,41 @@ const main = async () => {
     contract.on('WithdrawEvent', async(payee, amount) => {
         console.log(`on() ${payee} withdrew ${ethers.formatUnits(ethers.getBigInt(amount), 6)} ETH`);
     });
+
+    // 过滤监听
+    console.log("3. 利用 filters 持续监听 WithdrawEvent 事件")
+    let filter = contract.filters.WithdrawEvent('0x6b8C612F683D63BD358Cfcb72643a6959d917594', null);
+    console.log("过滤器详情:");
+    console.log(filter);
+    contract.on(filter, (res) => {
+        console.log('---------监听WithdrawEvent--------');
+        console.log(`filters ${res.args[0]} ${ethers.formatUnits(res.args[1], 6)}`);
+    });
+
+    // 过滤来自 myAddress 地址的 Transfer 事件
+    // const filter1 = contract.filters.Transfer(myAddress);
+    // 过滤所有发给 myAddress 地址的 Transfer 事件
+    // const filter2 = contract.filters.Transfer(null, myAddress);
+    // 过滤所有从 myAddress 发给 otherAddress的 Transfer 事件
+    // const filter3 = contract.filters.Transfer(myAddress, otherAddress);
+    // 过滤所有发给 myAddress 或 otherAddress 的 Transfer 事件
+    // const filter4 = contract.filters.Transfer(null, [ myAddress, otherAddress ]);
 }
 main()
+
+// 1. 利用 contract.once() 监听一次 WithdrawEvent 事件
+// 2. 利用 contract.on() 持续监听 WithdrawEvent 事件
+// 3. 利用 filters 持续监听 WithdrawEvent 事件
+// 过滤器详情:
+// PreparedTopicFilter {
+//   fragment: EventFragment {
+//     type: 'event',
+//     inputs: [ [ParamType], [ParamType] ],
+//     name: 'WithdrawEvent',
+//     anonymous: false
+//   }
+// }
+// once() 0x6b8C612F683D63BD358Cfcb72643a6959d917594 withdrew 0.0 ETH
+// on() 0x6b8C612F683D63BD358Cfcb72643a6959d917594 withdrew 0.0 ETH
+// ---------监听WithdrawEvent--------
+// filters 0x6b8C612F683D63BD358Cfcb72643a6959d917594 0.0
--- scripts/studyContractTest.js
+++ scripts/studyContractTest.js
@@ -27,3 +27,30 @@ const main = async () => {
     console.log("Withdraw transaction receipt:\n", withdrawTx);
 }
 main()
+
+// Account1 Address: 0x6b8C612F683D63BD358Cfcb72643a6959d917594
+// Withdrawing donated ETH from the contract to Account1...
+// Withdraw transaction receipt:
+//  ContractTransactionResponse {
+//   provider: JsonRpcProvider {},
+//   blockNumber: null,
+//   blockHash: null,
+//   index: undefined,
+//   hash: '0xfd691384a23b5c3d00903716e1187a05dbe2b104daebf9ad7631df3c3a9a0717',
+//   type: 2,
+//   to: '0xeF22976E24B7de69C9A979a7944dc72CF14BE089',
+//   from: '0x6b8C612F683D63BD358Cfcb72643a6959d917594',
+//   nonce: 30,
+//   gasLimit: 25774n,
+//   gasPrice: undefined,
+//   maxPriorityFeePerGas: 1000000n,
+//   maxFeePerGas: 8130741362n,
+//   maxFeePerBlobGas: null,
+//   data: '0x3ccfd60b',
+//   value: 0n,
+//   chainId: 11155111n,
+//   signature: Signature { r: 0x96cf201fb1bb1029832439d48883bb92583d4ad08ca2a6b3a9a47b57534c3bc9, s: 0x4d1badd978b04558802c191312ae923689f97f90ee78bb0cb80f023ded80fb3b, v: 27 },
+//   accessList: [],
+//   blobVersionedHashes: null,
+//   authorizationList: null
+// }
